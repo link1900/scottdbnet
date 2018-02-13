b0e7723972a256f3fb3c15d77f515e3d0b7df84a
Scott Brown <link1900@gmail.com>
2018-02-14 00:21:16 +1100

    Init commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Scott Brown
+      </div>
+    );
+  }
+}
+
+export default App;
