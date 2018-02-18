e18702f7bd45dc4727460f2bae4b22f6ff7eb7b6
Scott Brown <link1900@gmail.com>
2018-02-18 23:04:39 +1100

    Added flow, eslint and babel config

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import { Avatar, Card, CardContent, Divider, List, ListItem, ListItemText } from 'material-ui';
 import siteIcon from './icon.png';
 import gcaIcon from './gcaLogo.png';
 import linkedinLogo from './linkedin.png';
 import githubLogo from './github.png';
 import mailIcon from './mailIcon.png';
 import diceIcon from './diceIcon.png';
-import { Avatar, Card, CardContent, Divider, List, ListItem, ListItemText } from 'material-ui';
 
 class App extends Component {
+    navigateTo(url) {
+        window.location.href = url;
+    }
+
     render() {
         return (
             <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
@@ -19,33 +23,25 @@ class App extends Component {
                                 <ListItemText primary="Scott Brown" />
                             </ListItem>
                             <Divider />
-                            <ListItem
-                                button
-                                onClick={() => (window.location.href = 'https://linkin-games.firebaseapp.com/')}
-                            >
+                            <ListItem button onClick={() => this.navigateTo('https://linkin-games.firebaseapp.com/')}>
                                 <Avatar src={diceIcon} alt={'dice icon'} />
                                 <ListItemText primary="Games" />
                             </ListItem>
-                            <ListItem
-                                button
-                                onClick={() => (window.location.href = 'https://agra-ranker.herokuapp.com')}
-                            >
+                            <ListItem button onClick={() => this.navigateTo('https://agra-ranker.herokuapp.com')}>
                                 <Avatar src={gcaIcon} alt={'gca logo'} />
                                 <ListItemText primary="Greyhound Rankings" />
                             </ListItem>
-                            <ListItem button onClick={() => (window.location.href = 'mailto:link1900@gmail.com')}>
+                            <ListItem button onClick={() => this.navigateTo('mailto:link1900@gmail.com')}>
                                 <Avatar src={mailIcon} alt={'email icon'} />
                                 <ListItemText primary="Email" />
                             </ListItem>
-                            <ListItem button onClick={() => (window.location.href = 'https://github.com/link1900')}>
+                            <ListItem button onClick={() => this.navigateTo('https://github.com/link1900')}>
                                 <Avatar src={githubLogo} alt={'github logo'} />
                                 <ListItemText primary="Github" />
                             </ListItem>
                             <ListItem
                                 button
-                                onClick={() =>
-                                    (window.location.href = 'https://www.linkedin.com/in/scott-brown-246882b')
-                                }
+                                onClick={() => this.navigateTo('https://www.linkedin.com/in/scott-brown-246882b')}
                             >
                                 <Avatar src={linkedinLogo} alt={'linkedin logo'} />
                                 <ListItemText primary="Linkedin" />
