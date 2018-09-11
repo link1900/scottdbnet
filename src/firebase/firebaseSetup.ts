b8557a40f106912bb2b79bfa319aab12ff87f8be
Scott Brown <link1900@gmail.com>
2018-09-11 20:46:25 +1000

    fix: removed specific env information from source

--- src/firebase/firebaseSetup.ts
+++ src/firebase/firebaseSetup.ts
@@ -3,12 +3,12 @@ import { createStore, combineReducers, compose } from 'redux';
 import { reactReduxFirebase, firebaseReducer } from 'react-redux-firebase';
 
 export default firebase.initializeApp({
-    apiKey: 'AIzaSyAxWuNeT7IubNAGpMHYBXPteUNqun4cb-M',
-    authDomain: 'scottdbnet.firebaseapp.com',
-    databaseURL: 'https://scottdbnet.firebaseio.com',
-    projectId: 'scottdbnet',
-    storageBucket: 'scottdbnet.appspot.com',
-    messagingSenderId: '475516591163'
+    apiKey: process.env.REACT_APP_FIREBASE_WEB_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
 });
 
 const rrfConfig = {};
