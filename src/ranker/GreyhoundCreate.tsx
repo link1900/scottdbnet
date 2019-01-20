204435a88af766cb0eeb6fe99d329fa97deeb3d0
Scott Brown <link1900@gmail.com>
2019-01-20 16:07:27 +1100

    Added greyhound placeholders

---
+++ src/ranker/GreyhoundCreate.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PageFlow from '../UI/PageFlow';
+
+interface Props {}
+interface State {}
+
+export default class GreyhoundCreate extends React.Component<Props, State> {
+    public render() {
+        return (
+            <PageFlow>
+                <div>greyhound create go here</div>
+            </PageFlow>
+        );
+    }
+}
---
+++ src/ranker/GreyhoundEdit.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PageFlow from '../UI/PageFlow';
+
+interface Props {}
+interface State {}
+
+export default class GreyhoundEdit extends React.Component<Props, State> {
+    public render() {
+        return (
+            <PageFlow>
+                <div>greyhound edit go here</div>
+            </PageFlow>
+        );
+    }
+}
