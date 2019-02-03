def1cfd303e86c5a52fce719579c999a99758252
Scott Brown <link1900@gmail.com>
2019-02-03 16:05:59 +1100

    Added environment package

---
+++ functions/src/environment/environmentHelper.ts
@@ -0,0 +1,76 @@
+import InternalServerError from '../error/InternalServerError';
+
+export enum ExecutionEnvironment {
+  LOCAL_TEST = 'local-test',
+  LOCAL_DEV = 'local-dev',
+  DEV = 'dev',
+  TEST = 'test',
+  STAGING = 'staging',
+  PROD = 'prod'
+}
+
+export enum EnvironmentVariable {
+  DEPLOYMENT_ENVIRONMENT = 'DEPLOYMENT_ENV'
+}
+
+export function findVariable(key: string): string | undefined {
+  return process.env[key];
+}
+
+export function getVariable(key: string, defaultValue?: string): string {
+  const envVar = findVariable(key);
+  if (!envVar) {
+    if (defaultValue) {
+      return defaultValue;
+    }
+    throw new InternalServerError(`Environment variable ${key} was not found`);
+  }
+  return envVar;
+}
+
+export function getVariableAsInteger(key: string): number {
+  const stringValue = getVariable(key);
+  const result = parseInt(stringValue, 10);
+  if (!Number.isInteger(result)) {
+    throw new InternalServerError(`Environment variable ${key} was not a valid integer`);
+  }
+  return result;
+}
+
+export function isVariableEnabled(key: string): boolean {
+  try {
+    const result = getVariable(key);
+    return result.trim().toLowerCase() === 'true';
+  } catch (err) {
+    return false;
+  }
+}
+
+export function setVariable(envVarName: string, envVarValue: string, allowOverride: boolean = false): boolean {
+  if (allowOverride || !process.env[envVarName]) {
+    process.env[envVarName] = envVarValue;
+    return true;
+  }
+  return false;
+}
+
+export function getExecutionEnvironment(): ExecutionEnvironment {
+  const executionEnvironment = findVariable(EnvironmentVariable.DEPLOYMENT_ENVIRONMENT);
+
+  switch (executionEnvironment) {
+    case ExecutionEnvironment.LOCAL_TEST:
+      return ExecutionEnvironment.LOCAL_TEST;
+    case ExecutionEnvironment.LOCAL_DEV:
+      return ExecutionEnvironment.LOCAL_DEV;
+    case ExecutionEnvironment.DEV:
+      return ExecutionEnvironment.DEV;
+    case ExecutionEnvironment.TEST:
+      return ExecutionEnvironment.TEST;
+    case ExecutionEnvironment.STAGING:
+      return ExecutionEnvironment.STAGING;
+    case ExecutionEnvironment.PROD:
+      return ExecutionEnvironment.PROD;
+    default:
+      return ExecutionEnvironment.LOCAL_DEV;
+  }
+}
