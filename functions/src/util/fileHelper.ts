1cb6870ab1c8a9aa4de10eccf5a792945eab98a4
Scott Brown <link1900@gmail.com>
2019-02-03 15:09:05 +1100

    Fixed linting issues in fileHelper

--- functions/src/util/fileHelper.ts
+++ functions/src/util/fileHelper.ts
@@ -1,17 +1,25 @@
 import fs, { WriteFileOptions } from 'fs';
 
 export function readFileFromDisk(path: string) {
-    return new Promise((resolve, reject) => {
-        fs.readFile(path, (err, data) => {
-            return err ? reject(err) : resolve(data.toString());
-        });
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data.toString());
+      }
     });
+  });
 }
 
 export function writeFileToDisk(path: string, data: string, options: WriteFileOptions = {}) {
-    return new Promise((resolve, reject) => {
-        fs.writeFile(path, data, options, err => {
-            return err ? reject(err) : resolve(true);
-        });
+  return new Promise((resolve, reject) => {
+    fs.writeFile(path, data, options, err => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(true);
+      }
     });
+  });
 }
--- functions/src/util/promiseHelper.ts
+++ functions/src/util/promiseHelper.ts
@@ -1,79 +1,77 @@
 import BlueBird from 'bluebird';
 
-export interface MapPromiseOptions extends BasicMapPromiseOptions {
-    actionOnFailure?: (failureValue?: any, item?: any) => any;
-}
-
 export interface BasicMapPromiseOptions {
-    concurrency?: number;
-    delayInMilliseconds?: number;
+  concurrency?: number;
+  delayInMilliseconds?: number;
 }
 
 export interface WrappedPromise<T, R> {
-    state: boolean;
-    value: R;
-    item: T;
+  state: boolean;
+  value: R;
+  item: T;
 }
 
 export interface GroupedPromises<T> {
-    valid: T[];
-    invalid: any[];
+  valid: T[];
+  invalid: any[];
+}
+
+export interface MapPromiseOptions extends BasicMapPromiseOptions {
+  actionOnFailure?: (failureValue?: any, item?: any) => any;
 }
 
 export async function promiseEvery<T, R>(items: T[], func: (item: T) => Promise<R>, options?: MapPromiseOptions) {
-    const resultGroups = await promiseEveryGrouped(items, func, options);
-    if (resultGroups.invalid.length > 0 && options && options.actionOnFailure) {
-        resultGroups.invalid.forEach(invalidItem => {
-            if (options.actionOnFailure) {
-                return options.actionOnFailure(invalidItem.value, invalidItem.item);
-            }
-        });
-    }
-    return resultGroups.valid;
+  const resultGroups = await promiseEveryGrouped(items, func, options);
+  if (resultGroups.invalid.length > 0 && options && options.actionOnFailure) {
+    resultGroups.invalid.forEach(invalidItem => {
+      if (options.actionOnFailure) {
+        return options.actionOnFailure(invalidItem.value, invalidItem.item);
+      }
+    });
+  }
+  return resultGroups.valid;
 }
 
 export async function promiseEveryGrouped<T, R>(
-    items: T[],
-    func: (item: T) => Promise<R>,
-    options?: BasicMapPromiseOptions
+  items: T[],
+  func: (item: T) => Promise<R>,
+  options?: BasicMapPromiseOptions
 ): Promise<GroupedPromises<R>> {
-    const wrappedPromises = await promiseEveryWrapped(items, func, options);
-    return {
-        valid: wrappedPromises.filter(result => result.state).map(result => result.value),
-        invalid: wrappedPromises
-            .filter(result => !result.state)
-            .map(result => ({ value: result.value, item: result.item }))
-    };
+  const wrappedPromises = await promiseEveryWrapped(items, func, options);
+  return {
+    valid: wrappedPromises.filter(result => result.state).map(result => result.value),
+    invalid: wrappedPromises.filter(result => !result.state).map(result => ({ value: result.value, item: result.item }))
+  };
 }
 
 export async function promiseEveryWrapped<T, R>(
-    items: T[],
-    func: (item: T) => Promise<R>,
-    options?: BasicMapPromiseOptions
+  items: T[],
+  func: (item: T) => Promise<R>,
+  options?: BasicMapPromiseOptions
 ): Promise<Array<WrappedPromise<T, R>>> {
-    const concurrency = options && options.concurrency ? options.concurrency : 1;
-    const delayInMilliseconds = options && options.delayInMilliseconds ? options.delayInMilliseconds : 0;
+  const concurrency = options && options.concurrency ? options.concurrency : 1;
+  const delayInMilliseconds = options && options.delayInMilliseconds ? options.delayInMilliseconds : 0;
 
-    return BlueBird.map(
-        items,
-        async item => {
-            try {
-                return {
-                    state: true,
-                    value: await delayPromise(func(item), delayInMilliseconds),
-                    item
-                };
-            } catch (error) {
-                return { state: false, value: error, item };
-            }
-        },
-        { concurrency }
-    );
+  return BlueBird.map(
+    items,
+    async item => {
+      try {
+        return {
+          state: true,
+          value: await delayPromise(func(item), delayInMilliseconds),
+          item
+        };
+      } catch (error) {
+        return { state: false, value: error, item };
+      }
+    },
+    { concurrency }
+  );
 }
 
 export async function delayPromise<T>(promise: T, delayInMilliseconds: number): Promise<T> {
-    if (delayInMilliseconds <= 0) {
-        return promise;
-    }
-    return BlueBird.resolve(promise).delay(delayInMilliseconds);
+  if (delayInMilliseconds <= 0) {
+    return promise;
+  }
+  return BlueBird.resolve(promise).delay(delayInMilliseconds);
 }
