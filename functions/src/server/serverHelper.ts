06e9532e5b5ed2d2886e50dfca656b5b08b9c8d7
Scott Brown <link1900@gmail.com>
2019-03-12 19:04:48 +1100

    Added helpers for setting the roles on a context

--- functions/src/server/serverHelper.ts
+++ functions/src/server/serverHelper.ts
@@ -1,8 +1,18 @@
 import 'reflect-metadata';
+import { Connection } from 'typeorm';
 import { getVariable, isVariableEnabled } from '../environment/environmentHelper';
 import InternalServerError from '../error/InternalServerError';
 import { closeConnection, connectToDatabase } from '../database/databaseHelper';
-import { Connection } from 'typeorm';
+import ServerContext from './ServerContext';
+import { createDataLoaders } from './dataLoaders';
+import logger from '../logging/logger';
+import { get } from 'lodash';
+import { Request } from 'express';
+import { IncomingHttpHeaders } from 'http';
+import { User } from './User';
+import * as admin from 'firebase-admin';
+import DecodedIdToken = admin.auth.DecodedIdToken;
+import { Role } from './Role';
 
 let connection: Connection | undefined = undefined;
 
@@ -42,4 +52,53 @@ export async function closeDatabaseConnection(): Promise<boolean> {
     return true;
   }
   return false;
+}
+
+export async function createContextFromRequest(req: Request): Promise<ServerContext> {
+  const currentConnection = await getDatabaseConnection();
+  const loaders = await createDataLoaders(currentConnection);
+  // create context
+  const serverContext = new ServerContext(loaders);
+
+  // populate with user credentials
+  const idToken = await getIdTokenFromHeader(req.headers);
+  if (idToken) {
+    try {
+      serverContext.idToken = idToken;
+      const decodedIdToken = await admin.auth().verifyIdToken(idToken);
+      logger.trace('ID Token correctly decoded', decodedIdToken);
+      serverContext.decodedIdToken = decodedIdToken;
+      serverContext.user = getUserFromDecodedIdToken(serverContext.decodedIdToken);
+      serverContext.roles = getRolesForUser(serverContext.user);
+    } catch (error) {
+      logger.error('Error while verifying ID Token:', error);
+    }
+  }
+  return serverContext;
+}
+
+export async function getIdTokenFromHeader(headers: IncomingHttpHeaders): Promise<string | undefined> {
+  if (headers.authorization && headers.authorization.startsWith('Bearer ')) {
+    logger.trace('Found Authorization header');
+    return headers.authorization.split('Bearer ')[1];
+  }
+
+  return undefined;
+}
+
+export function getUserFromDecodedIdToken(decodedIdToken: DecodedIdToken): User {
+  return {
+    id: decodedIdToken.user_id,
+    name: decodedIdToken.name,
+    email: decodedIdToken.email,
+    imageUrl: decodedIdToken.picture
+  };
+}
+
+export function getRolesForUser(user: User): Role[] {
+  if (user.id) {
+    return [Role.ADMIN, Role.USER];
+  }
+
+  return [];
 }
