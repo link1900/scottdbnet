83d478258ab01665c7887c007d61cd1289468320
Scott Brown <link1900@gmail.com>
2019-03-24 12:37:23 +1100

    Setting log level from env var

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,11 @@
 import * as functions from 'firebase-functions';
 import { getApolloServer } from './server/cloudFunctionHelper';
-import { loadEnvironmentVariablesFromConfig } from './environment/environmentHelper';
+import { getVariable, loadEnvironmentVariablesFromConfig } from './environment/environmentHelper';
+import logger from './logging/logger';
 
 process.env.EXECUTION_ENVIRONMENT = functions.config().core.executionenvironment;
 loadEnvironmentVariablesFromConfig();
+logger.logLevel = getVariable('LOG_LEVEL');
 const apolloServer = getApolloServer();
 
 export const graphql = functions.https.onRequest(apolloServer.createHandler());
