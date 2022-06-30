db01d88e53a6343ded1084433ea5f17ab868b203
Scott Brown <link1900@gmail.com>
2022-06-30 17:15:27 +1000

    Changed localstore key for list randomizer

--- src/tools/listRandomizer/ListRandomizer.tsx
+++ src/tools/listRandomizer/ListRandomizer.tsx
@@ -15,7 +15,7 @@ import CloseIcon from "@material-ui/icons/Close";
 import MoodIcon from "@material-ui/icons/Mood";
 import ClearIcon from "@material-ui/icons/Clear";
 import { useLocation } from "react-router-dom";
-import { useLocalStorage } from 'react-use';
+import { useLocalStorage } from "react-use";
 import { makeValidJsonRequest } from "../../util/apiHelper";
 import {
   base64DecodeString,
@@ -58,15 +58,18 @@ function getUrlStore(urlSearch: string): ListRandomizerStore | undefined {
 export function ListRandomizer() {
   const { search } = useLocation();
   const urlStore = getUrlStore(search);
-  const [localStore, setLocalStore, removeLocalStore] = useLocalStorage<ListRandomizerStore>('listRandomizer');
-  const [store, setStore] = React.useState<ListRandomizerStore>(urlStore?? localStore ?? defaultStore);
+  const [localStore, setLocalStore, removeLocalStore] =
+    useLocalStorage<ListRandomizerStore>("listRandomizer.store");
+  const [store, setStore] = React.useState<ListRandomizerStore>(
+    urlStore ?? localStore ?? defaultStore
+  );
   const [toastOpen, setToastOpen] = React.useState(false);
   const [joke, setJoke] = useState<string>("");
 
   const updateStore = (updatedStore: ListRandomizerStore) => {
     setLocalStore(updatedStore);
     setStore(updatedStore);
-  }
+  };
 
   const updateList = (updatedList: string[]) => {
     updateStore({
@@ -82,7 +85,7 @@ export function ListRandomizer() {
       textValue: updateText,
       listValue: stringToListItems(updateText)
     });
-  }
+  };
 
   const handleTextChange = (event: any) => {
     const textValue = event.target.value;
@@ -90,7 +93,7 @@ export function ListRandomizer() {
   };
 
   const handleShuffle = () => {
-    updateList(shuffle(store.listValue))
+    updateList(shuffle(store.listValue));
   };
 
   const handleSort = () => {
@@ -120,7 +123,7 @@ export function ListRandomizer() {
   const handleReset = () => {
     updateStore(defaultStore);
     removeLocalStore();
-  }
+  };
 
   const generateJoke = async () => {
     try {
@@ -134,107 +137,107 @@ export function ListRandomizer() {
   };
 
   return (
-      <Grid container justifyContent="center" spacing={2}>
-        <Snackbar
-          anchorOrigin={{
-            vertical: "bottom",
-            horizontal: "left"
-          }}
-          open={toastOpen}
-          autoHideDuration={6000}
-          onClose={handleClose}
-          message="Link copied to clipboard"
-          action={
-            <IconButton
-              size="small"
-              aria-label="close"
-              color="inherit"
-              onClick={handleClose}
-            >
-              <CloseIcon fontSize="small" />
-            </IconButton>
-          }
-        />
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <Grid container direction={"column"} spacing={2}>
-            <Grid item>
-              <Grid container direction="row" spacing={1}>
-                <Grid item>
-                  <Button
-                    variant="contained"
-                    size="small"
-                    color="primary"
-                    startIcon={<ShuffleIcon />}
-                    onClick={() => handleShuffle()}
-                  >
-                    Shuffle
-                  </Button>
-                </Grid>
-                <Grid item>
-                  <Button
-                    variant="outlined"
-                    size="small"
-                    color="primary"
-                    startIcon={<SortIcon />}
-                    onClick={() => handleSort()}
-                  >
-                    Sort
-                  </Button>
-                </Grid>
-                <Grid item>
-                  <Button
-                    variant="outlined"
-                    size="small"
-                    color="primary"
-                    startIcon={<ShareIcon />}
-                    onClick={() => handleShare()}
-                  >
-                    Share
-                  </Button>
-                </Grid>
-                <Grid item>
-                  <Button
-                    variant="outlined"
-                    size="small"
-                    color="primary"
-                    startIcon={<MoodIcon />}
-                    onClick={() => generateJoke()}
-                  >
-                    Joke
-                  </Button>
-                </Grid>
-                <Grid item>
-                  <Button
-                    variant="outlined"
-                    size="small"
-                    color="primary"
-                    startIcon={<ClearIcon />}
-                    onClick={() => handleReset()}
-                  >
-                    Reset
-                  </Button>
-                </Grid>
-              </Grid>
-            </Grid>
-            <Grid item>
-              <TextField
-                id="simple-item-input"
-                label="Items"
-                multiline
-                fullWidth
-                minRows={20}
-                value={store.textValue}
-                onChange={handleTextChange}
-                variant="outlined"
-              />
-            </Grid>
-            {joke.length > 0 ? (
+    <Grid container justifyContent="center" spacing={2}>
+      <Snackbar
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left"
+        }}
+        open={toastOpen}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        message="Link copied to clipboard"
+        action={
+          <IconButton
+            size="small"
+            aria-label="close"
+            color="inherit"
+            onClick={handleClose}
+          >
+            <CloseIcon fontSize="small" />
+          </IconButton>
+        }
+      />
+      <Grid item xs={12} sm={6} md={4} lg={3}>
+        <Grid container direction={"column"} spacing={2}>
+          <Grid item>
+            <Grid container direction="row" spacing={1}>
               <Grid item>
-                <Typography>{joke}</Typography>
+                <Button
+                  variant="contained"
+                  size="small"
+                  color="primary"
+                  startIcon={<ShuffleIcon />}
+                  onClick={() => handleShuffle()}
+                >
+                  Shuffle
+                </Button>
               </Grid>
-            ) : null}
+              <Grid item>
+                <Button
+                  variant="outlined"
+                  size="small"
+                  color="primary"
+                  startIcon={<SortIcon />}
+                  onClick={() => handleSort()}
+                >
+                  Sort
+                </Button>
+              </Grid>
+              <Grid item>
+                <Button
+                  variant="outlined"
+                  size="small"
+                  color="primary"
+                  startIcon={<ShareIcon />}
+                  onClick={() => handleShare()}
+                >
+                  Share
+                </Button>
+              </Grid>
+              <Grid item>
+                <Button
+                  variant="outlined"
+                  size="small"
+                  color="primary"
+                  startIcon={<MoodIcon />}
+                  onClick={() => generateJoke()}
+                >
+                  Joke
+                </Button>
+              </Grid>
+              <Grid item>
+                <Button
+                  variant="outlined"
+                  size="small"
+                  color="primary"
+                  startIcon={<ClearIcon />}
+                  onClick={() => handleReset()}
+                >
+                  Reset
+                </Button>
+              </Grid>
+            </Grid>
           </Grid>
+          <Grid item>
+            <TextField
+              id="simple-item-input"
+              label="Items"
+              multiline
+              fullWidth
+              minRows={20}
+              value={store.textValue}
+              onChange={handleTextChange}
+              variant="outlined"
+            />
+          </Grid>
+          {joke.length > 0 ? (
+            <Grid item>
+              <Typography>{joke}</Typography>
+            </Grid>
+          ) : null}
         </Grid>
       </Grid>
+    </Grid>
   );
 }
